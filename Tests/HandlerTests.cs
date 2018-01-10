2d6d87ddb501822e28c90b15e97a8c25c8cfb73b
PK <karthi.email@gmail.com>
2018-01-11 01:53:29 +1100

    Fixed logging - closes #3
    travis build is fixed as well - close #4

--- Tests/HandlerTests.cs
+++ Tests/HandlerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.TestUtilities;
+using Domain;
 using Handlers;
 using Xunit;
 
@@ -8,7 +9,7 @@ namespace Tests
     public class HandlerTests
     {
         [Fact]
-        public void TestHandler()
+        public void TestHealthCheck()
         {
             var handler = new Handler();
 
@@ -19,6 +20,18 @@ namespace Tests
 
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("OK", response.Body);
+        }
+
+        [Fact]
+        public void TestHello()
+        {
+            var handler = new Handler();
+
+            var context = new TestLambdaContext();
+
+            var response = handler.Hello(new Request(), context);
+
+            Assert.NotNull(response);
         }
     }
 }
